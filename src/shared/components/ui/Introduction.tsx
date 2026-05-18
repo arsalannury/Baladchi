b3ceda98465056300715fd7573b3254f39c2dc7d
arsalannury <arsiiiland@yahoo.com>
2026-05-18 22:28:34 +0330

    Test ui with add Box component

--- src/shared/components/ui/Introduction.tsx
+++ src/shared/components/ui/Introduction.tsx
@@ -1,11 +1,23 @@
+import Box from "../common/Box";
+
 const Introduction = () => {
   return (
     <>
       <div className="">
         <p className="text-right text-2xl">
-          چجور <span className="text-sky-600 border-b-[4px] border-b-sky-600">کمک</span> هایی میتونی اینجا
-          بگیری؟
+          چجور{" "}
+          <span className="text-sky-600 border-b-4 border-b-sky-600">کمک</span>{" "}
+          هایی میتونی اینجا بگیری؟
         </p>
+        <div className="flex items-center justify-around mt-10">
+          <Box boxTitle="کمک کن" />
+          <Box boxTitle="کمک کن" />
+          <Box boxTitle="کمک کن" />
+          <Box boxTitle="کمک کن" />
+          <Box boxTitle="کمک کن" />
+          <Box boxTitle="کمک کن" />
+          <Box boxTitle="کمک کن" />
+        </div>
       </div>
     </>
   );
