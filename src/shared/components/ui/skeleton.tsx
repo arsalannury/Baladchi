44e68750fd75c6ac782f221477573898615ce18f
arsalannury <arsiiiland@yahoo.com>
2026-06-12 17:23:17 +0330

    - Add variant and line into component

--- src/shared/components/ui/skeleton.tsx
+++ src/shared/components/ui/skeleton.tsx
@@ -1,13 +1,55 @@
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
+import React from "react";
 
-function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
+type SkeletonProps = React.ComponentProps<"div"> & {
+  variant?: "text" | "circle" | "rectangle";
+  lines?: number;
+};
+
+function Skeleton({
+  className,
+  variant = "rectangle",
+  lines = 1,
+  ...props
+}: SkeletonProps) {
+  // specify for text variant
+  if (variant === "text") {
+    return (
+      <div className="space-y-2">
+        {Array.from({ length: lines }).map((_, i) => (
+          <div
+            key={i}
+            className={cn(
+              "h-11 w-full animate-pulse rounded-md bg-muted",
+              className,
+            )}
+            {...props}
+          />
+        ))}
+      </div>
+    );
+  }
+
+  // specify for circle variant
+  if (variant === "circle") {
+    return (
+      <div
+        className={cn(
+          "h-10 w-10 animate-pulse rounded-full bg-muted",
+          className,
+        )}
+        {...props}
+      />
+    );
+  }
+
+  // specify for default style
   return (
     <div
-      data-slot="skeleton"
-      className={cn("animate-pulse rounded-md bg-muted", className)}
+      className={cn("h-4 w-full animate-pulse rounded-md bg-muted", className)}
       {...props}
     />
-  )
+  );
 }
 
-export { Skeleton }
+export { Skeleton };
