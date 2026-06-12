8d3c329958dbef16eee1b400fe5b0dd05ecc001a
arsalannury <arsiiiland@yahoo.com>
2026-06-12 16:23:24 +0330

    - Add spinner component with native css styles

--- src/shared/components/ui/Spinner.tsx
+++ src/shared/components/ui/Spinner.tsx
@@ -1,9 +1,9 @@
 const Spinner = () => {
-    return ( 
-        <>
-        
-        </>
-     );
-}
- 
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+      <span className="loader"></span>
+    </div>
+  );
+};
+
 export default Spinner;
