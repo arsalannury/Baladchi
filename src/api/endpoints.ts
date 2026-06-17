3fccc5598dc8b9b71deec1aa561b9c657a205e5b
arsalannury <arsiiiland@yahoo.com>
2026-06-17 23:00:19 +0330

    - Add new endpoint address

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -2,4 +2,5 @@ export abstract class Endpoints {
   static readonly GET_TRENDING_TOPICS = "trendingTopics";
   static readonly GET_BEST_CONTIBUTORS = "bestContributors";
   static readonly GET_QUESTIONS = "questions";
+  static readonly GET_ALL_TOPICS = "topics";
 }
