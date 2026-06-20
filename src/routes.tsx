9cef3816028a68b157ec9ce1e64c1e82f126e67b
arsalannury <arsiiiland@yahoo.com>
2026-06-20 22:02:53 +0330

    - Add route for detail topic

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router";
 import Layout from "./shared/layout/Layout";
 import Home from "./pages/Home";
 import Topics from "./pages/Topics";
+import Topic from "./pages/Topic";
 
 const routes = createBrowserRouter([
   {
@@ -10,6 +11,7 @@ const routes = createBrowserRouter([
     children: [
       { index: true, Component: Home },
       { path: "topics", Component: Topics },
+      { path: "topics/:id", Component: Topic },
       { path: "*", Component: Home },
     ],
   },
