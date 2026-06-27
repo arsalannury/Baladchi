6e1108bc61ddabe9eefd162f99c782eff3cc2b9f
arsalannury <arsiiiland@yahoo.com>
2026-06-27 18:50:00 +0330

    - Update endpoint

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -3,4 +3,5 @@ export abstract class Endpoints {
   static readonly GET_BEST_CONTIBUTORS = "bestContributors";
   static readonly GET_QUESTIONS = "questions";
   static readonly GET_ALL_TOPICS = "topics";
- }
+  static readonly GET_TOP_EXPERTS = "topExperts";
+}
